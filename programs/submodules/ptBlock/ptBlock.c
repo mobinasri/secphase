b73937a48ce3b7f87b7d1690ce7140d78d6d78f3
Mobin Asri <masri@ucsc.edu>
2023-04-25 13:42:48 -0700

    Fixed a minor bug

--- programs/submodules/ptBlock/ptBlock.c
+++ programs/submodules/ptBlock/ptBlock.c
@@ -368,7 +368,7 @@ void ptBlock_add_marker_blocks_by_contig(stHash *blocks_per_contig, char* contig
     for (int i =0; i < stList_length(markers); i++){
         ptMarker* marker = stList_get(markers, i);
         // add marker block only if its alignment index matches the given index
-        if(marker->alignment->idx == alignment_idx) {
+        if(marker->alignment_idx == alignment_idx) {
             stList_append(blocks, ptMarker_convert_to_block(marker));
         }
     }
